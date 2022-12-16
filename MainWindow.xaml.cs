fcfe59e0abf657d9bf6faf2783b31a0cf0ed4e72
Krzysztof Niedźwiedzki <K.Niedzwiedzki@student.zschie.edu.pl>
2022-12-16 14:15:53 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();///chowa aktualne okno
+            Window1 x = new Window1();/// tworzę funkcje x odwołującą się do aktualnego okna
+            x.ShowDialog();/// pokazuje nowe okno
+            this.Close();/// zamykam poprzednie oknko, które zostało schowane
+        }
+    }
+}
