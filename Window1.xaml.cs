b4be881a340359355b79b93ac3bd2ab7ce3364af
Krzysztof Niedźwiedzki <K.Niedzwiedzki@student.zschie.edu.pl>
2022-12-16 14:44:09 +0100

    dodanie wstepnego kodu laczenia i wyszukiwania z baza danych

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -29,5 +29,22 @@ namespace Biblioteka
             x.ShowDialog();/// pokazuje nowe okno
             this.Close();/// zamykam poprzednie oknko, które zostało schowane
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            /*string myConnection = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\student\source\repos\LibraryProject\LibraryProject\Biblioteka.mdf; Integrated Security = True";
+            SqlConnection myConn = new SqlConnection(myConnection);
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+            SqlCommandBuilder cb = new SqlCommandBuilder(myDataAdapter);
+            myConn.Open();
+            DataSet ds = new DataSet();
+
+            SqlCommand cmd = new SqlCommand(myConnection);
+            cmd.Connection = myConn;
+            cmd.CommandText = "INSERT INTO [books](Tytul, Autor, Gatunek) values ('" + tytul.Text + "','" + autor.Text + "','" + gatunek.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            myConn.Close();*/
+        }
     }
 }
