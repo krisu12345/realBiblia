aa0e1dade031267de8e1f853f3860602c6f215ba
Visual Student <visualstudent@zschie.edu.pl>
2022-12-20 12:29:30 +0100

    nie dziala tutaj w szkole sql using jakos

--- Window3.xaml.cs
+++ Window3.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,32 +36,31 @@ namespace Biblioteka
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            /*string myConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\student\\source\\repos\\LibraryProject\\LibraryProject\\Biblioteka.mdf;Integrated Security=True";
+            string myConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\student\\source\\repos\\realBiblia\\Database1.mdf;Integrated Security=True";
             SqlConnection myConn = new SqlConnection(myConnection);
             SqlDataAdapter myDataAdapter = new SqlDataAdapter();
             SqlCommandBuilder cb = new SqlCommandBuilder(myDataAdapter);
             myConn.Open();
             DataSet ds = new DataSet();
 
-            SqlCommand cmd = new SqlCommand("SELECT * FROM [books]", myConn);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [Table]", myConn);
             cmd.Connection = myConn;
 
             var reader = cmd.ExecuteReader();
 
-             wyszukane.Text = "";
+             wypisz.Text = "";
 
             while (reader.Read())
              {
-                 string line = $"{reader["Tytul"]} {reader["Autor"]} {reader["Gatunek"]} {reader["Id"]}";
+                 string line = $"{reader["Tytul"]} {reader["Autor"]}{reader["Id"]}";
                 if (
-                    reader["Tytul"].ToString().Contains(tytul_box.Text)&&
-                    reader["Autor"].ToString().Contains(autor_box.Text)&&
-                    reader["Gatunek"].ToString().Contains(gatunek_box.Text))
-                    wyszukane.AppendText(line + "\r\n");
+                    reader["Tytul"].ToString().Contains(tytul1.Text)&&
+                    reader["Autor"].ToString().Contains(autor1.Text)&&
+                    wypisz.AppendText(line + "\r\n"));
              }
             
 
-        myConn.Close();*/
+        myConn.Close();
         }
     }
 }
